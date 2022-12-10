330da49aa5d16ae04c0f2176c8dc04dfa9b6c016
drmotsei <drmotsei@gmail.com>
2022-12-09 18:58:49 -0800

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x = '0';
+	int y = '0';
+
+	for (y = '0'; x <= '9'; y++)
+	{
+		for (x = '0'; x <= '9'; x++)
+		{
+			if (!((x == y) || (y > x)))
+			{
+				putchar(y);
+				putchar(x);
+				if (!(x == '9' && y == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
