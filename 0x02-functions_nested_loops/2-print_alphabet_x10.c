af25e6afc3fcad7f65747e66720f7934918d229f
drmotsei <drmotsei@gmail.com>
2022-12-13 17:25:06 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@ void print_alphabet_x10(void)
 	char c;
 
 	for (ten = 0; ten <= 9; ten++)
-	{
+	
 		for (c = 'a'; c <= 'z'; c++)
-		{
+		
 			_putchar(c);
-		}
+		
 		_putchar('\n');
-	}
+	
 	return (0);
 }
 
