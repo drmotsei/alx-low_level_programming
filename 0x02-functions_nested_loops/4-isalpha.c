63751bc47248a36ddcaabc2b01b06994daeda846
drmotsei <drmotsei@gmail.com>
2022-12-13 18:17:22 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *_isalpha - Entry
- *Retrun: Returns 1 c is a letter, otherwise 0
+ *Return: Returns 1 c is a letter, otherwise 0
  *@c: returns 1 if either an upper or lower case otherwise returns 0
  */
 int _isalpha(int c)
