a0039f656c1e1b190d5c51e0c5e1794f0deb7cda
drmotsei <drmotsei@gmail.com>
2022-12-22 17:55:27 -0800

    0x06

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,12 +4,12 @@
  * @n: pointer
  * Return: n
  */
-char *string_toupper(char *n)
+char *string_toupper(char *ni)
 {
 	int i;
 
 	i = 0;
-	while (n[i] != 0'\0')
+	while (n[i] != '\0')
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
