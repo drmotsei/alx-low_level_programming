1452880de38dd3b9fc94375a163a539d4954fbd4
drmotsei <drmotsei@gmail.com>
2023-01-05 18:01:54 -0800

    Advanced

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
+int iterate_wild(char **wildstr);
 char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
 /**
