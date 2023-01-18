466d52fa882ea21f13d689407628d6833b4f8011
drmotsei <drmotsei@gmail.com>
2023-01-18 13:24:39 -0800

    0x0F. C - Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	int bytes, i;
 	char *arr;
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
-		{	
+		{
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
